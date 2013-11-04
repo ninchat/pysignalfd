864ee38d1f2c043d23661087d6c96f9ebe51edfa
Timo Savola <timo@ninchat.com>
2013-11-04 14:29:58 +0200

    iterate, enqueue helpers

--- signalfd/__init__.py
+++ signalfd/__init__.py
@@ -1,8 +1,43 @@
 __all__ = [
+	"enqueue",
 	"init",
+	"iterate",
 	"mask",
 	"read",
 	"reset",
 ]
 
+import errno
+import select
+import signal
+
 from _signalfd import *
+
+def iterate():
+	fd = init()
+
+	try:
+		while True:
+			try:
+				r, _, _ = select.select([fd], [], [])
+			except select.error as e:
+				if e.args[0] == errno.EINTR:
+					continue
+				raise
+
+			if r:
+				signum = read()
+				if signum > 0:
+					yield signum
+	finally:
+		reset()
+
+def enqueue(queue, wrapper=identity, terminator=StopIteration):
+	try:
+		for signum in iterate():
+			queue.put(wrapper(signum))
+	finally:
+		queue.put(terminator)
+
+def identity(signum):
+	return signum
